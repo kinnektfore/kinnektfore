d2812fe3904283667b7ecbd9c2cdf72416438164
Harry Rigg <harryrigg14@gmail.com>
2020-12-17 11:53:23 +0800

    fix another one of alexes mistakes

--- static/index.js
+++ static/index.js
@@ -1,7 +1,8 @@
-var socket = io();
-
-socket.emit('join room', 1234);
 $('document').ready(() => {
+
+    var socket = io();
+    socket.emit('join room', 1234);
+
     var i;
     for (i = 0; i < 4; i++) {
         console.log('cool')
