369586ca465bb64a523a26d9e21cfb331dfb807f
Alex Hawking <44057725+Handmade-Studios@users.noreply.github.com>
2020-12-17 11:54:57 +0800

    scripts in the body :-(

--- static/index.js
+++ static/index.js
@@ -4,6 +4,8 @@ socket.emit('join room', 1234);
 $('document').ready(() => {
     var i;
     for (i = 0; i < 4; i++) {
-        console.log('cool')
+        for (j = 0; j < 4; j++) {
+            console.log('cool')
+        }
     }
 })
