48af7585214c199862506ef6d1e7631e6d786056
Harry Rigg <harryrigg14@gmail.com>
2020-12-17 11:24:35 +0800

    add serverside socket

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const port = 3000;
 
 app.use(express.static('static'));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+
 });
